1bde766961e2197ef17d7b3a17bebd479997ac6b
josephigorpi <131630561+josephigorpi@users.noreply.github.com>
2026-05-04 21:45:52 -0500

    Update nuevo_triaje.py

--- frontend/streamlit/pages/nuevo_triaje.py
+++ frontend/streamlit/pages/nuevo_triaje.py
@@ -1,3 +1,8 @@
+import sys
+import os
+# Primero el path, luego tus módulos
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+
 import streamlit as st
 import pandas as pd
 import requests

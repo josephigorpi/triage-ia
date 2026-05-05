2dd3fa9ad555394b9ef765cdab5bb907df2df103
josephigorpi <131630561+josephigorpi@users.noreply.github.com>
2026-05-04 21:39:27 -0500

    Configure system path for module imports
    Add system path configuration to import modules from project root.

--- frontend/streamlit/app.py
+++ frontend/streamlit/app.py
@@ -2,6 +2,13 @@ import streamlit as st
 from utils import verificar_usuario, registrar_log
 import pandas as pd
 
+import sys
+import os
+
+# Esto le dice a Python que busque módulos en la raíz del proyecto
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+
 st.set_page_config(page_title="Triaje IA", layout="wide")
 
 # Inicializar estado de sesión
